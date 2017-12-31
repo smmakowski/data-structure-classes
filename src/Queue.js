4491215522fe6ce2d7362ba379f96020c37a87c1
smmakowski <makowski.sm@gmail.com>
2017-12-30 23:51:00 -0800

    (init) init npm; setup class skeletons; install deps; add test js

---
+++ src/Queue.js
@@ -0,0 +1,9 @@
+module.exports = class Queue {
+  constructor() {
+    this.value = 'what';
+  }
+
+  logVal() {
+    console.log(this.value);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const Stack = require('./Stack.js');
+const Queue = require('./Queue.js');
+const LinkedList = require('./LinkedList.js');
+const Tree = require('./Tree');
+const Graph = require('./Graph.js');
+const BinarySearchTree = require('./BinarySearchTree.js');
+
+module.exports = {
+  Stack,
+  Queue
+}
